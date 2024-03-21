d4c5df0a4d2f0f5d4442169aeb35d14ec6cb081d
CMcGinty1 <147287709+CMcGinty1@users.noreply.github.com>
2024-03-21 10:35:20 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿//task 1
+console.log("Connor McGinty");
+
+//task 2
+console.log("1997");
+
+//task 3
+console.log(1997);
+
+//task 4
+console.log("Connor McGinty 1997");
+console.log("Connor", "McGinty", 1997);
+
+//task 5
+console.log("Connor");
+console.log("McGinty");
+console.log(1997);
+
+//task 6
+console.log("Connor" + "McGinty" + 1997);
+
+//task 7
+console.log("Connor " + "McGinty " + 1997);
